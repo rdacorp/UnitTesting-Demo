7c33cbd4cba4af68728815325045c89fa09f8bd7
Nick Berardi <nberardi@gmail.com>
2014-06-19 10:41:04 -0400

    syncing between lab and demo

--- src/Todo.Lab/Models/TodoItem.cs
+++ src/Todo.Lab/Models/TodoItem.cs
@@ -14,5 +14,28 @@ namespace Todo.Models
 
 		public string Title { get; set; }
 		public bool Completed { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((TodoItem)obj);
+		}
+
+		protected bool Equals(TodoItem other)
+		{
+			return Id == other.Id && String.Equals(Title, other.Title) && Completed.Equals(other.Completed);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked {
+				int hashCode = Id;
+				hashCode = (hashCode * 397) ^ (Title != null ? Title.GetHashCode() : 0);
+				hashCode = (hashCode * 397) ^ Completed.GetHashCode();
+				return hashCode;
+			}
+		}
 	}
 }
